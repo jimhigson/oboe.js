5a7a2bebdafea637d7ee3d4b586dfb05cd1644b0
Jim Higson <jim@wikizzle.org>
2013-04-05 12:44:50 +0100

    allow multi-pattern adding for onfind

--- src/main/oboe.js
+++ src/main/oboe.js
@@ -149,20 +149,20 @@ var oboe = (function(oboe){
       return this;
    };
 
-   /**
-    * returns a function which tests if a listener is interested in the given path
-    */
-   function matchesPath( path ) {
-   
-      return function( listener ) {
-         return listener.pattern.test( path );         
-      };
-   } 
    
    /**
     * notify any of the listeners that are interested in the path.       
     */  
    function notifyListeners ( listenerList, foundNode, path ) {
+
+      /**
+       * returns a function which tests if a listener is interested in the given path
+       */
+      function matchesPath( path ) {      
+         return function( listener ) {
+            return listener.pattern.test( path );         
+         };
+      } 
 
       listenerList.filter(matchesPath(path))
          .forEach( function(listener) {
@@ -235,19 +235,19 @@ var oboe = (function(oboe){
    /**
     * Add a new json path to the parser, which will be called when a value is found at the given path
     *
-    * @param {String} jsonPath
-    *    supports these special meanings:
-    *          //                - root json object
-    *          /                 - path separator
-    *          *                 - any named node in the path
-    *          **                - any number of intermediate nodes (non-greedy)
+    * @param {String} jsonPath supports the same syntax as .onPath.
     *
     * @param {Function} callback
     * @param {Object} [context] the scope for the callback
     */
    OboeParser.prototype.onFind = function (jsonPath, callback, context) {
 
-      pushListener(this._thingFoundListeners, jsonPath, callback, context);
+      if( typeof jsonPath === 'string' ) {
+         pushListener(this._thingFoundListeners, jsonPath, callback, context);
+      } else {
+         pushListeners(this._thingFoundListeners, jsonPath);
+      }
+
       return this;
    };
    
