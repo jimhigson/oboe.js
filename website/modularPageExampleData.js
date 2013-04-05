ae3fb573530cc28bca54d49a481fae2e9d18734b
Jim Higson <jim@wikizzle.org>
2013-04-05 13:00:49 +0100

    add a very early start of an example page

---
+++ website/modularPageExampleData.js
@@ -0,0 +1,32 @@
+var dataTemplate = 
+{
+
+   "pageInfo": {
+   }
+
+,  "barChart": {
+   }
+   
+,  "imageSlider":{
+   }
+   
+,  "map": {
+      "minLat"    :""
+   ,  "minLong"   :""
+   ,  "maxLat"    :""
+   ,  "maxLong"   :""
+   ,  "points"    : [
+      ]   
+   }      
+
+,  "table": {
+      "heading"   :"A table of some data"
+   ,  "data"      : [
+         {heading:'Weight', value:'20kg'}
+      ,  {heading:'', value:''}
+      ,  {heading:'', value:''}
+      ,  {heading:'', value:''}
+      ]
+   }
+            
+};
