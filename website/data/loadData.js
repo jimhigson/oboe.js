6ee8e09c524e1ce07359604422228726b93f7a95
Jim Higson <jim@wikizzle.org>
2013-05-01 12:07:08 +0100

    take a bit more off the global namespace

--- website/data/loadData.js
+++ website/data/loadData.js
@@ -1,69 +1,76 @@
 
+/* The purpose of this file is to simulate a slow-loading ajax call of some template-based data */
+var FakeAjax = (function(){
 
-function expandJsonTemplate( json ) {
-            
-   // traveerse json recursively, replacing some special tokens with random values:
-   
-   function replacePlaceholders(templateString){
-      return templateString.replace(
-         "{{Boolean}}", 
-         function(){ return Math.random() > 0.5 }
-      );
-   }
-   
-   for( var i in json ) {
-   
-      switch( json[i].constructor.name ) {
-         
-         case "Object":
-         case "Array":         
-            json[i] = expandJsonTemplate(json[i]);
-            break;
-         
-         case "String":
-            json[i] = replacePlaceholders( json[i] );
-            break;                       
+   /** The json template has placeholders like {{Boolean}} or {{Name}}. Traverse it and replace them to make
+    *  a sample page. 
+    */
+   function expandJsonTemplate( json ) {
+               
+      // traveerse json recursively, replacing some special tokens with random values:
+      
+      function replacePlaceholders(templateString){
+         return templateString.replace(
+            "{{Boolean}}", 
+            function(){ return Math.random() > 0.5 }
+         );
       }
-   }
-                  
-   return json;         
-}
-
-/** I can't remember if Javascript objects are deterministic in their ordering, but in practice
- *  they always output to JSON in the order that the keys were added. Make things a bit more
- *  interesting by randomising that order
- */
-function randomiseMapOrder(map) {
-   var randomkeys = _.shuffle( _.keys(map) ),
-       newMap = {};
-   
-   _.each( randomkeys, function( key ) {
-      newMap[key] = map[key];      
-   });
-   
-   return newMap;
-}      
-
-var FakeAjax = {
-   loadThrottled: function loadThrottled(dripSize, dripInterval, callback) {
       
-      var fullData = JSON.stringify(randomiseMapOrder(expandJsonTemplate(dataTemplate)));      
-         
-      var cursorPosition = 0;         
-         
-      var intervalId = window.setInterval(function(){
+      for( var i in json ) {
       
-         var nextDrip = fullData.substr(cursorPosition, dripSize);         
-         
-         cursorPosition += dripSize;
-                        
-         callback(nextDrip);                     
-         
-         if( cursorPosition >= fullData.length ) {
-            window.clearInterval(intervalId);
-         }                                                        
-   
-      }, dripInterval);
+         switch( json[i].constructor.name ) {
+            
+            case "Object":
+            case "Array":         
+               json[i] = expandJsonTemplate(json[i]);
+               break;
+            
+            case "String":
+               json[i] = replacePlaceholders( json[i] );
+               break;                       
+         }
+      }
+                     
+      return json;         
    }
-};
+   
+   /** I can't remember if Javascript objects are deterministic in their ordering, but in practice
+    *  they always output to JSON in the order that the keys were added. Make things a bit more
+    *  interesting by randomising that order
+    */
+   function randomiseMapOrder(map) {
+      var randomkeys = _.shuffle( _.keys(map) ),
+          newMap = {};
+      
+      _.each( randomkeys, function( key ) {
+         newMap[key] = map[key];      
+      });
+      
+      return newMap;
+   }      
+   
+   return {
+      loadThrottled: function loadThrottled(dripSize, dripInterval, callback) {
+         
+         var fullData = JSON.stringify(randomiseMapOrder(expandJsonTemplate(dataTemplate)));      
+            
+         var cursorPosition = 0;         
+            
+         var intervalId = window.setInterval(function(){
+         
+            var nextDrip = fullData.substr(cursorPosition, dripSize);         
+            
+            cursorPosition += dripSize;
+                           
+            callback(nextDrip);                     
+            
+            if( cursorPosition >= fullData.length ) {
+               window.clearInterval(intervalId);
+            }                                                        
+      
+         }, dripInterval);
+      }
+   };
+
+}());
 
