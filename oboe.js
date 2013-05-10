04939c2654cd2035c8a19afb9b8e03fc315a6e6f
Jim Higson <jim@wikizzle.org>
2013-05-10 15:40:06 +0100

    not sure that closure was really making anthing clearer anymore

--- oboe.js
+++ oboe.js
@@ -659,196 +659,188 @@ function partialComplete( fn /* arg1, arg2, arg3 ... */ ) {
 
 
 var jsonPathCompiler = (function () {
-
-   /** 
-    * tokenExprs is A list of functions representing features in the jsonPath expression. For each function, 
-    * if a given string matches the pattern for that language feature, it will return a generated parser for that 
-    * expression. Otherwise, returns undefined. 
-    */
-   var tokenExprs = (function(){
    
-      /**
-       * Expression for:
-       *    *
-       *    [*]
-       *    
-       * Normally, this would be compiled into a jsonPath parser by partially completing
-       *    previousExpr, capturing, name to give a function which takes just the particularities
-       *    of the path being evaluated, pathStack, nodeStack, stackIndex. All other expr functions
-       *    follow this pattern too.
-       * 
-       * @returns {Object|false} either the object that was found, or false if nothing was found         
-       */
-      function unnamedNodeExpr(previousExpr, capturing, _nameless, pathStack, nodeStack, stackIndex ){
-      
-         // a '*' doesn't put any extra criteria on the matching, it just defers to the previous expression:
-         var previous = previousExpr(pathStack, nodeStack, stackIndex-1);                   
-                  
-         return previous && returnValueForMatch(capturing, previous, nodeStack, stackIndex);
-      }
-      
-      /**
-       * Expression for a named path node, expressed as:
-       *    foo
-       *    ["foo"]
-       *    [2]
-       * 
-       * @returns {Object|false} either the object that was found, or false if nothing was found
-       */
-      function namedNodeExpr(previousExpr, capturing, name, pathStack, nodeStack, stackIndex ) {
-                                               
-         // in implementation, is like unnamednodeExpr except that we need the name to match.
-         // Once name matches, defer to unnamedNodeExpr:                                                                  
-         return (pathStack[stackIndex] == name) && unnamedNodeExpr(previousExpr, capturing, name, pathStack, nodeStack, stackIndex );               
-      }      
-      
-      /**
-       * Expression for .. (double dot) token              
-       * 
-       * @returns {Object|false} either the object that was found, or false if nothing was found         
-       */   
-      function multipleUnnamedNodesExpr(previousExpr, _neverCaptures, _nameless, pathStack, nodeStack, stackIndex ) {
+   /**
+    * Expression for:
+    *    *
+    *    [*]
+    *    
+    * Normally, this would be compiled into a jsonPath parser by partially completing
+    *    previousExpr, capturing, name to give a function which takes just the particularities
+    *    of the path being evaluated, pathStack, nodeStack, stackIndex. All other expr functions
+    *    follow this pattern too.
+    * 
+    * @returns {Object|false} either the object that was found, or false if nothing was found         
+    */
+   function unnamedNodeExpr(previousExpr, capturing, _nameless, pathStack, nodeStack, stackIndex ){
+   
+      // a '*' doesn't put any extra criteria on the matching, it just defers to the previous expression:
+      var previous = previousExpr(pathStack, nodeStack, stackIndex-1);                   
                
-         // past the start, not a match:
-         if( stackIndex < -1 ) {
-            return false;
-         }
-      
-         return stackIndex == -1 || // -1 is the root 
-                previousExpr(pathStack, nodeStack, stackIndex) || 
-                multipleUnnamedNodesExpr(previousExpr, _neverCaptures, _nameless, pathStack, nodeStack, stackIndex-1);         
-      }      
-      
-      /**
-       * Expression for $ - matches only the root element of the json
-       * 
-       * @returns {Object|false} either the object that was found, or false if nothing was found         
-       */   
-      function rootExpr(_cantHaveExprsBeforeRoot, capturing, _nameless, pathStack, nodeStack, stackIndex ){
-         return stackIndex == -1 && returnValueForMatch(capturing, true, nodeStack, stackIndex);
-      }   
-      
-      /**
-       * Expression for . does no tests since . is just a separator. Just passes through to the
-       * next function in the chain.
-       * 
-       * @returns {Object|false} either the object that was found, or false if nothing was found         
-       */   
-      function passthroughExpr(previousExpr, _neverCaptures, _nameless, pathStack, nodeStack, stackIndex) {
-         return previousExpr(pathStack, nodeStack, stackIndex);
-      }   
-      
-      /**
-       * Expression for the empty string. As the jsonPath parser generates the path parser, it will eventually
-       * run out of tokens and get to the empty string. So, all generated parsers will be wrapped in this function.
-       * 
-       * Initialises the stackIndex and kicks off the other expressions.   
-       * 
-       * @returns {Object|false} either the object that was found, or false if nothing was found         
-       */   
-      function statementExpr(startingExpr, _neverCaptures, _nameless, pathStack, nodeStack){
-      
-         // kick off the parsing by passing through to the first expression with the stackIndex set to the
-         // top of the stack:
-         var exprMatch = startingExpr(pathStack, nodeStack, pathStack.length-1);
-                               
-         // Returning exactly true indicates that there has been a match but no node is captured. 
-         // By default, the node at the top of the stack gets returned. Just like in css4 selector 
-         // spec, if there is no $, the last node in the selector is the one being styled.                      
-                         
-         return exprMatch === true ? lastOf(nodeStack) : exprMatch;
-      }      
-                 
-      /** extraction of some common logic used by expression when they have matched.
-       *  If is a capturing node, will return it's item on the nodestack. Otherwise, will return the item
-       *  from the nodestack given by the previous expression, or true if none
-       */
-      function returnValueForMatch(capturing, previousExprEvaluation, nodeStack, stackIndex) {
-         return capturing? nodeStack[stackIndex+1] : (previousExprEvaluation || true);
-      }
-
-
-      /**
-       * For when a token match has been found. Compiles the parser for that token.
-       * 
-       * @param {Function} expr the expression that parses this token 
-       * @param {Function} parserGeneratedSoFar the parser already found
-       * @param tokenMatch the match given by the regex engine when the token was found
-       */
-      function compileTokenToParser( expr, parserGeneratedSoFar, tokenMatch ) {
-         var capturing = !!tokenMatch[1],
-             name = tokenMatch[2];      
+      return previous && returnValueForMatch(capturing, previous, nodeStack, stackIndex);
+   }
+   
+   /**
+    * Expression for a named path node, expressed as:
+    *    foo
+    *    ["foo"]
+    *    [2]
+    * 
+    * @returns {Object|false} either the object that was found, or false if nothing was found
+    */
+   function namedNodeExpr(previousExpr, capturing, name, pathStack, nodeStack, stackIndex ) {
+                                            
+      // in implementation, is like unnamednodeExpr except that we need the name to match.
+      // Once name matches, defer to unnamedNodeExpr:                                                                  
+      return (pathStack[stackIndex] == name) && unnamedNodeExpr(previousExpr, capturing, name, pathStack, nodeStack, stackIndex );               
+   }      
+   
+   /**
+    * Expression for .. (double dot) token              
+    * 
+    * @returns {Object|false} either the object that was found, or false if nothing was found         
+    */   
+   function multipleUnnamedNodesExpr(previousExpr, _neverCaptures, _nameless, pathStack, nodeStack, stackIndex ) {
             
-         return partialComplete( expr, parserGeneratedSoFar, capturing, name);      
+      // past the start, not a match:
+      if( stackIndex < -1 ) {
+         return false;
       }
+   
+      return stackIndex == -1 || // -1 is the root 
+             previousExpr(pathStack, nodeStack, stackIndex) || 
+             multipleUnnamedNodesExpr(previousExpr, _neverCaptures, _nameless, pathStack, nodeStack, stackIndex-1);         
+   }      
+   
+   /**
+    * Expression for $ - matches only the root element of the json
+    * 
+    * @returns {Object|false} either the object that was found, or false if nothing was found         
+    */   
+   function rootExpr(_cantHaveExprsBeforeRoot, capturing, _nameless, pathStack, nodeStack, stackIndex ){
+      return stackIndex == -1 && returnValueForMatch(capturing, true, nodeStack, stackIndex);
+   }   
+   
+   /**
+    * Expression for . does no tests since . is just a separator. Just passes through to the
+    * next function in the chain.
+    * 
+    * @returns {Object|false} either the object that was found, or false if nothing was found         
+    */   
+   function passthroughExpr(previousExpr, _neverCaptures, _nameless, pathStack, nodeStack, stackIndex) {
+      return previousExpr(pathStack, nodeStack, stackIndex);
+   }   
+   
+   /**
+    * Expression for the empty string. As the jsonPath parser generates the path parser, it will eventually
+    * run out of tokens and get to the empty string. So, all generated parsers will be wrapped in this function.
+    * 
+    * Initialises the stackIndex and kicks off the other expressions.   
+    * 
+    * @returns {Object|false} either the object that was found, or false if nothing was found         
+    */   
+   function statementExpr(startingExpr, _neverCaptures, _nameless, pathStack, nodeStack){
+   
+      // kick off the parsing by passing through to the first expression with the stackIndex set to the
+      // top of the stack:
+      var exprMatch = startingExpr(pathStack, nodeStack, pathStack.length-1);
+                            
+      // Returning exactly true indicates that there has been a match but no node is captured. 
+      // By default, the node at the top of the stack gets returned. Just like in css4 selector 
+      // spec, if there is no $, the last node in the selector is the one being styled.                      
+                      
+      return exprMatch === true ? lastOf(nodeStack) : exprMatch;
+   }      
+              
+   /** extraction of some common logic used by expression when they have matched.
+    *  If is a capturing node, will return it's item on the nodestack. Otherwise, will return the item
+    *  from the nodestack given by the previous expression, or true if none
+    */
+   function returnValueForMatch(capturing, previousExprEvaluation, nodeStack, stackIndex) {
+      return capturing? nodeStack[stackIndex+1] : (previousExprEvaluation || true);
+   }
 
-      /** If jsonPath matches the given regular expression pattern, return a partially completed version of expr
-       *  which is ready to be used as a jsonPath parser. 
-       *  
-       *  This function is designed to be partially completed with the pattern and expr, leaving a function
-       *  which can be stored in the tokenExprs array. tokenExpr(pattern, expr) is a shorthand for this
-       *  partial completion.
-       *  
-       *  Returns undefined on no match
-       *  
-       * @param {RegExp} pattern
-       * @param {Function} expr
-       * @param {String} jsonPath
-       * @param {Function} parserGeneratedSoFar
-       * 
-       * @param {Function(Function, String)} onSuccess a function to pass the generated parser to if one can be made,
-       *    also passes the remaining string from jsonPath that is still to parse
-       * 
-       * @return {*|undefined}
-       */
-      function compileTokenToParserIfMatches(pattern, expr, jsonPath, parserGeneratedSoFar, onSuccess) {
-         var tokenMatch = pattern.exec(jsonPath);
 
-         if(tokenMatch) {
-            var compiledParser = compileTokenToParser(expr, parserGeneratedSoFar, tokenMatch),
-                remaining = jsonPath.substr(tokenMatch[0].length);                
-                                
-             // partially complete the expression with the previous expr, the capturing flag and the name
-             // (some exprs ignore some of these params)  
-            return onSuccess(remaining, compiledParser);
-         }         
-      }
-                 
-      /**
-       * Generate a function which parses the pattern in the given regex. If matches, returns a parser
-       * generated from that token that processes the given expr, otherwise returns null.
-       * 
-       * @returns {Function(Function parserGeneratedSoFar, Function onSucess)}
-       */
-      function tokenExpr(pattern, expr) {
-      
-         return partialComplete( compileTokenToParserIfMatches, pattern, expr );
-      }     
-      
-      var nameInObjectNotation    = /^(\$?)(\w+)/    
-      ,   nameInArrayNotation     = /^(\$?)\["(\w+)"\]/         
-      ,   numberInArrayNotation   = /^(\$?)\[(\d+)\]/
-      ,   starInObjectNotation    = /^(\$?)\*/
-      ,   starInArrayNotation     = /^(\$?)\[\*\]/      
-      ,   doubleDot               = /^\.\./
-      ,   dot                     = /^\./      
-      ,   bang                    = /^(\$?)!/
-      ,   emptyString             = /^$/;
-      
-      // A list of functions which test if a string matches the required patter and, if it does, returns
-      // a generated parser for that expression      
-      return [
-         tokenExpr(nameInObjectNotation   , namedNodeExpr),
-         tokenExpr(nameInArrayNotation    , namedNodeExpr),         
-         tokenExpr(numberInArrayNotation  , namedNodeExpr),
-         tokenExpr(starInObjectNotation   , unnamedNodeExpr),
-         tokenExpr(starInArrayNotation    , unnamedNodeExpr),         
-         tokenExpr(doubleDot              , multipleUnnamedNodesExpr),
-         tokenExpr(dot                    , passthroughExpr),         
-         tokenExpr(bang                   , rootExpr),             
-         tokenExpr(emptyString            , statementExpr)
-      ];
-   })(); // end of tokenExprs definition
+   /**
+    * For when a token match has been found. Compiles the parser for that token.
+    * 
+    * @param {Function} expr the expression that parses this token 
+    * @param {Function} parserGeneratedSoFar the parser already found
+    * @param tokenMatch the match given by the regex engine when the token was found
+    */
+   function compileTokenToParser( expr, parserGeneratedSoFar, tokenMatch ) {
+      var capturing = !!tokenMatch[1],
+          name = tokenMatch[2];      
+         
+      return partialComplete( expr, parserGeneratedSoFar, capturing, name);      
+   }
+
+   /** If jsonPath matches the given regular expression pattern, return a partially completed version of expr
+    *  which is ready to be used as a jsonPath parser. 
+    *  
+    *  This function is designed to be partially completed with the pattern and expr, leaving a function
+    *  which can be stored in the tokenExprs array. tokenExpr(pattern, expr) is a shorthand for this
+    *  partial completion.
+    *  
+    *  Returns undefined on no match
+    *  
+    * @param {RegExp} pattern
+    * @param {Function} expr
+    * @param {String} jsonPath
+    * @param {Function} parserGeneratedSoFar
+    * 
+    * @param {Function(Function, String)} onSuccess a function to pass the generated parser to if one can be made,
+    *    also passes the remaining string from jsonPath that is still to parse
+    * 
+    * @return {*|undefined}
+    */
+   function compileTokenToParserIfMatches(pattern, expr, jsonPath, parserGeneratedSoFar, onSuccess) {
+      var tokenMatch = pattern.exec(jsonPath);
+
+      if(tokenMatch) {
+         var compiledParser = compileTokenToParser(expr, parserGeneratedSoFar, tokenMatch),
+             remaining = jsonPath.substr(tokenMatch[0].length);                
+                             
+          // partially complete the expression with the previous expr, the capturing flag and the name
+          // (some exprs ignore some of these params)  
+         return onSuccess(remaining, compiledParser);
+      }         
+   }
+              
+   /**
+    * Generate a function which parses the pattern in the given regex. If matches, returns a parser
+    * generated from that token that processes the given expr, otherwise returns null.
+    * 
+    * @returns {Function(Function parserGeneratedSoFar, Function onSucess)}
+    */
+   function tokenExpr(pattern, expr) {
+   
+      return partialComplete( compileTokenToParserIfMatches, pattern, expr );
+   }     
+   
+   var nameInObjectNotation    = /^(\$?)(\w+)/    
+   ,   nameInArrayNotation     = /^(\$?)\["(\w+)"\]/         
+   ,   numberInArrayNotation   = /^(\$?)\[(\d+)\]/
+   ,   starInObjectNotation    = /^(\$?)\*/
+   ,   starInArrayNotation     = /^(\$?)\[\*\]/      
+   ,   doubleDot               = /^\.\./
+   ,   dot                     = /^\./      
+   ,   bang                    = /^(\$?)!/
+   ,   emptyString             = /^$/;
+   
+   // A list of functions which test if a string matches the required patter and, if it does, returns
+   // a generated parser for that expression      
+   var tokenExprs = [
+      tokenExpr(nameInObjectNotation   , namedNodeExpr),
+      tokenExpr(nameInArrayNotation    , namedNodeExpr),         
+      tokenExpr(numberInArrayNotation  , namedNodeExpr),
+      tokenExpr(starInObjectNotation   , unnamedNodeExpr),
+      tokenExpr(starInArrayNotation    , unnamedNodeExpr),         
+      tokenExpr(doubleDot              , multipleUnnamedNodesExpr),
+      tokenExpr(dot                    , passthroughExpr),         
+      tokenExpr(bang                   , rootExpr),             
+      tokenExpr(emptyString            , statementExpr)
+   ];
 
 
    /**
