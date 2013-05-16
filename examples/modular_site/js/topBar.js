d2fe7292efda133c17e69685b825c1a8cdaae5b5
Jim Higson <jim@wikizzle.org>
2013-05-16 12:13:31 +0100

    start to fix double click bug in example

--- examples/modular_site/js/topBar.js
+++ examples/modular_site/js/topBar.js
@@ -2,12 +2,16 @@
 function initTopBar( requestOboe ) {
    "use strict";
    
-   var button = $('#loadButton').click(function(){
+   $('#loadButton').click(loadNewFakeJson);
+      
+   function loadNewFakeJson(){
    
       var jsonToLoad = JSON.stringify(RandomJson.randomiseMapOrder(RandomJson.expandJsonTemplate(dataTemplate)));
    
+      requestOboe.reset();
+   
       // ok, let's simulate a slow connection and feed the response into our oboe:
       FakeAjax.fetch(jsonToLoad, 5, 5, requestOboe.read.bind(requestOboe));
-   });
+   }   
    
 }   
