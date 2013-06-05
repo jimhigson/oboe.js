49427fc1f209d48067d5f507c4276a567d3f8ef7
Jim Higson <jim@wikizzle.org>
2013-06-05 18:34:18 +0100

    shortcut prototypes in polyfills

--- src/libs/polyfills.js
+++ src/libs/polyfills.js
@@ -1,52 +1,56 @@
-/**
- * Here we have a fairly minimal set of polyfills needed to give old and rubbish browsers a fighting
- * chance of running the code. IE8, I'm looking at you.
- * 
- * If you already have polyfills in your webapp or you don't need to support bad browsers, feel free 
- * to make a custom build without this.
- */
+(function(arrayProto, functionProto){
 
-if( !Array.prototype.forEach ) {
-
-   /* Array.forEach has to be a polyfill, clarinet expects it
-    *  Ignoring all but function argument since not needed, eg can't take a context  */     
-   Array.prototype.forEach = function( func ){
+   /**
+    * Here we have a fairly minimal set of polyfills needed to give old and rubbish browsers a fighting
+    * chance of running the code. IE8, I'm looking at you.
+    * 
+    * If you already have polyfills in your webapp or you don't need to support bad browsers, feel free 
+    * to make a custom build without this.
+    */
    
-      for( var i = 0 ; i < this.length ; i++ ) {      
-         func( this[i] );    
-      }      
-   };         
-}
-
-if( !Array.prototype.filter ) {
-
-   /* Array.filter has to be a polyfill, clarinet expects it.
-    *  Ignoring all but function argument since not needed, eg can't take a context  */ 
-   Array.prototype.filter = function( func ){
-         
-      var out = [];
+   if( !arrayProto.forEach ) {
    
-      // let's use the .forEach we just declared above to implement .filter
-      this.forEach(function(item){      
-         if( func( item ) ) {
-            out.push(item);
-         }                  
-      });
+      /* Array.forEach has to be a polyfill, clarinet expects it
+       *  Ignoring all but function argument since not needed, eg can't take a context  */     
+      arrayProto.forEach = function( func ){
       
-      return out;
-   };
-}
-
-if( !Function.prototype.bind ) {
- 
-   Function.prototype.bind = function( context /*, arg1, arg2 ... */ ){
-      var f = this,
-          boundArgs = toArray(arguments, 1);
+         for( var i = 0 ; i < this.length ; i++ ) {      
+            func( this[i] );    
+         }      
+      };         
+   }
    
-      return function( /* yet more arguments */ ) {
-         var callArgs = boundArgs.concat(toArray(arguments));            
+   if( !arrayProto.filter ) {
+   
+      /* Array.filter has to be a polyfill, clarinet expects it.
+       *  Ignoring all but function argument since not needed, eg can't take a context  */ 
+      arrayProto.filter = function( func ){
             
-         return f.apply(context, callArgs);
-      }
-   };
-}
+         var out = [];
+      
+         // let's use the .forEach we just declared above to implement .filter
+         this.forEach(function(item){      
+            if( func( item ) ) {
+               out.push(item);
+            }                  
+         });
+         
+         return out;
+      };
+   }
+   
+   if( !functionProto.bind ) {
+    
+      functionProto.bind = function( context /*, arg1, arg2 ... */ ){
+         var f = this,
+             boundArgs = toArray(arguments, 1);
+      
+         return function( /* yet more arguments */ ) {
+            var callArgs = boundArgs.concat(toArray(arguments));            
+               
+            return f.apply(context, callArgs);
+         }
+      };
+   }
+
+})(Array.prototype, Function.prototype);
