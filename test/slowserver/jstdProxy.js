5f0a5f9ac12a43c5886aa8be58d1932ad00d03fa
Jim Higson <jim@wikizzle.org>
2013-06-06 14:29:16 +0100

    make proxy work on any domain not just localhost

--- test/slowserver/jstdProxy.js
+++ test/slowserver/jstdProxy.js
@@ -1,24 +1,41 @@
 require('colors');
 
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+var proxy = new httpProxy.RoutingProxy();
+
 var JSTD_PORT = 4224,
     STREAM_PORT = 4567;
-    PROXY_PORT = 2442;
-
-var proxyOptions = {
-        router: {
-          'localhost/stream': '127.0.0.1:' + STREAM_PORT,
-          'localhost/':       '127.0.0.1:' + JSTD_PORT
-        }
-};
+PROXY_PORT = 2442;
 
 console.log("jstd will be started on port".yellow, String(JSTD_PORT).blue,
-             "but connect browsers via the proxy on port".yellow, String(PROXY_PORT).blue,
-             "eg by going to".yellow, ("http://localhost:" + PROXY_PORT).blue, 'and clicking capture'.yellow,
-             "\nThe proxy is necessary for asynchronous http testing which will fail otherwise".red );
-             
-console.log('creating proxy server on port'.yellow, String(PROXY_PORT).blue);
-console.log('routing table for proxy is'.yellow, JSON.stringify(proxyOptions.router).blue);
+    "but connect browsers via the proxy on port".yellow, String(PROXY_PORT).blue,
+    "eg by going to".yellow, ("http://localhost:" + PROXY_PORT).blue, 'and clicking capture'.yellow,
+    "\nThe proxy is necessary for asynchronous http testing which will fail otherwise".red);
 
-require('http-proxy').createServer(proxyOptions).listen(PROXY_PORT);
+console.log('creating proxy server on port'.yellow, String(PROXY_PORT).blue);
+
+//console.log('routing table for proxy is'.yellow, JSON.stringify(proxyOptions.router).blue);
+
+//
+// Create a regular http server and proxy its handler
+//
+http.createServer(
+    function (req, res) {
+    
+       var url = req.url,
+           destinationPort = (url.indexOf('/stream') === 0) ? STREAM_PORT : JSTD_PORT;
+
+       console.log('proxy got request for'.yellow, url.blue, 
+                    'will redirect to'.green, 'localhost:'.yellow, String(destinationPort).blue);
+
+       proxy.proxyRequest(req, res, {
+          host:'localhost',
+          port:destinationPort
+       });
+
+    }).listen(PROXY_PORT);
+
 
 console.log('proxy started'.green, 'now capture some browsers!');
