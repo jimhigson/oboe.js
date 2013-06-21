a3a1ae4ddce4d07893cc3b400ece292ef4418ed6
Jim Higson <jim@wikizzle.org>
2013-06-21 19:07:07 +0100

    new built versions

--- dist/oboe.js
+++ dist/oboe.js
@@ -15,6 +15,10 @@ function toArray(arrayLikeThing, startIndex) {
    return Array.prototype.slice.call(arrayLikeThing, startIndex);
 }
 
+function isString(thing) {
+   return typeof thing == 'string';
+}
+
 /*
    Call each of a list of functions with the same arguments, ignoring any return
    values.
@@ -697,8 +701,7 @@ if(typeof FastList === 'function') {
  *
  * TODO:
  *    error handling
- *    allow setting of request params and other such options
- *    x-browser testing, compatibility
+ *    allow setting of request params
  */
 var streamingXhr = (function (XHR) {
    
@@ -750,6 +753,7 @@ var streamingXhr = (function (XHR) {
     * 
     * @param {String} method one of 'GET' 'POST' 'PUT' 'DELETE'
     * @param {String} url
+    * @param {String|null} data
     * @param {Function(String nextResponseDrip)} progressCallback
     *    A callback to be called repeatedly as the input comes in.
     *    Will be passed the new string since the last call.
@@ -766,7 +770,7 @@ var streamingXhr = (function (XHR) {
           numberOfCharsGivenToCallback = 0;
 
       xhr.open(method, url, true);
-      xhr.send(data || null);
+      xhr.send(data);
 
       function handleProgress() {
          
@@ -1299,7 +1303,7 @@ var oboe = (function(){
    var oboeProto = OboeParser.prototype;
 
    /**
-    * Ask this oboe instance to fetch the given url
+    * Ask this oboe instance to fetch the given url. Called via one of the public api methods.
     * 
     * @param {String} url
     * @param {Function (String|Array wholeParsedJson)} doneCallback a callback for when the request is
@@ -1309,6 +1313,9 @@ var oboe = (function(){
    oboeProto._fetch = function(method, url, data, doneCallback) {
       var self = this;
 
+      // data must either be a string or null to give to streamingXhr as the request body:
+      data = data? (isString(data)? data: JSON.stringify(data)) : null;      
+
       streamingXhr(
          method,
          url, 
@@ -1317,7 +1324,7 @@ var oboe = (function(){
          function() {            
             self.close();
             
-            (doneCallback || always)(self._jsonBuilder.getRoot());                                          
+            doneCallback && doneCallback(self._jsonBuilder.getRoot());                                          
          });
                
       return self;
@@ -1462,7 +1469,7 @@ var oboe = (function(){
    /** implementation behind .onPath() and .onFind 
     */
    function on(listenerList, jsonPath, callback, context) {
-      if( typeof jsonPath === 'string' ) {
+      if( isString(jsonPath) ) {
          pushListener(listenerList, jsonPath, callback, context);
       } else {
          pushListeners(listenerList, jsonPath);
@@ -1547,16 +1554,10 @@ var oboe = (function(){
           method = oboeProto[apiMethodName] =
              
             function(firstArg) {
-            
+
                var url, data, doneCallback;
-            
-               if( typeof firstArg == 'object' ) {
-                  // parameters specified as options object:
-                  url = firstArg.url;
-                  data = firstArg.data;
-                  doneCallback = firstArg.complete; 
-                  
-               } else {
+
+               if (isString(firstArg)) {
                   // parameters specified as arguments
                   //
                   //  if mayHaveContext, signature is:
@@ -1566,10 +1567,15 @@ var oboe = (function(){
                   //                                
                   url = firstArg;
                   data = arguments[dataArgumentIndex];
-                  doneCallback =  arguments[callbackArgumentIndex]
+                  doneCallback = arguments[callbackArgumentIndex]
+               } else {
+                  // parameters specified as options object:
+                  url = firstArg.url;
+                  data = firstArg.data;
+                  doneCallback = firstArg.complete;
                }
-               
-               return this._fetch(httpMethodName, url, data, doneCallback );         
+
+               return this._fetch(httpMethodName, url, data, doneCallback);
             };   
       
       // make the above method available without creating an oboe instance first via
