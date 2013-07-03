5d3c8a34ee4442bdb087a4b136d1265ad4c074dc
Jim Higson <jim@wikizzle.org>
2013-07-03 13:19:52 +0100

    reorganise tokens

--- src/jsonPathTokens.js
+++ src/jsonPathTokens.js
@@ -4,14 +4,59 @@
 // allowing new token regexes to match.
 //    For all regular expressions:
 //       The first subexpression is the $ (if the token is eligible to capture)
-//       The second subexpression is the name of the expected path node (if the token may have a name)               
-var jsonPathNamedNodeInObjectNotation     = /^(\$?)(\w+)/             //  foo
-,   jsonPathNamedNodeInArrayNotation      = /^(\$?)\["(\w+)"\]/       //  ["foo"]    
-,   jsonPathNumberedNodeInArrayNotation   = /^(\$?)\[(\d+)\]/         //  [2]
-,   jsonPathStarInObjectNotation          = /^(\$?)\*/                //  [*]
-,   jsonPathStarInArrayNotation           = /^(\$?)\[\*\]/            //  *
-,   jsonPathDoubleDot                     = /^\.\./                   //  ..
-,   jsonPathDot                           = /^\./                     //  .
-,   jsonPathBang                          = /^(\$?)!/                 //  !
-,   emptyString                           = /^$/                      // nada!
+//       The second subexpression is the name of the expected path node (if the token may have a name)
+
+var regexSource = partialComplete(pluck, 'source');
+
+function r(componentRegexes) {
+
+   return RegExp( componentRegexes.map(regexSource).join('') );
+}
+
+function jsonPathClause() {
+   
+   var strings = toArray(arguments);
+   
+   strings.unshift(/^/);
+   
+   return r(strings);
+}
+
+function unquotedArrayNotation(contents) {
+   return r([/\[/, contents, /\]/]);
+}
+
+var possiblyCapturing =           /(\$?)/
+,   namedNode =                   /(\w+)/
+,   namedNodeInArrayNotation =    /\["(\w+)"\]/
+,   numberedNodeInArrayNotation = unquotedArrayNotation( /(\d+)/ )
+,   anyNodeInArrayNotation =      unquotedArrayNotation( /\*/ )
+;    
+               
+var jsonPathNamedNodeInObjectNotation     = jsonPathClause(possiblyCapturing, namedNode)
+//                                           /^(\$?)(\w+)/             //   foo
+
+,   jsonPathNamedNodeInArrayNotation      = jsonPathClause(possiblyCapturing, namedNodeInArrayNotation)
+//                                           /^(\$?)\["(\w+)"\]/       //   ["foo"]
+    
+,   jsonPathNumberedNodeInArrayNotation   = jsonPathClause(possiblyCapturing, numberedNodeInArrayNotation)
+//                                           /^(\$?)\[(\d+)\]/         //   [2]
+
+,   jsonPathStarInObjectNotation          = jsonPathClause(possiblyCapturing, /\*/)
+//                                           /^(\$?)\*/                //   *
+
+,   jsonPathStarInArrayNotation           = jsonPathClause(possiblyCapturing, anyNodeInArrayNotation)
+//                                           /^(\$?)\[\*\]/            //   [*]
+
+,   jsonPathDoubleDot                     = jsonPathClause(/\.\./)
+//                                           /^\.\./                   //   ..
+
+,   jsonPathDot                           = jsonPathClause(/\./)
+//                                           /^\./                     //   .
+
+,   jsonPathBang                          = jsonPathClause(possiblyCapturing, /!/)
+//                                           /^(\$?)!/                 //   !
+
+,   emptyString                           = jsonPathClause(/$/)
+//                                           /^$/                      //   nada!
 ;  
