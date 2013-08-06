591641f7f7c8d60f06d09e5b2f2d9b8e873e7b5f
Jim Higson <jim@wikizzle.org>
2013-08-06 17:17:50 +0100

    move debugging output for tests into own file

--- test/cases/browserApiTest.js
+++ test/cases/browserApiTest.js
@@ -18,41 +18,6 @@ function streamingXhrShouldHaveBeenGiven(/* arguments */) {
       return streamingStub.alwaysCalledWithMatch.apply( streamingStub, expectedArgs );    
    }
    
-   function anyToString(thing){
-      
-      if( thing === null ) {
-         return 'null';
-      }
-      
-      if( thing === undefined ) {
-         return 'undefined';
-      }
-
-      if( typeof thing == 'string' ) {
-         return '(string)' + thing;
-      }
-      
-      if( thing.constructor == Array ) {
-         return '[' + thing.map(anyToString).join(', ') + ']';
-      }      
-
-      if( typeof thing == 'function' ) {
-         return thing.name? 'function ' + thing.name : 'anon function'; 
-      }
-      
-      if( typeof thing == 'object' ) {
-            
-         return (
-                  thing.constructor == Object? 
-                     '' 
-                  :  '(' + (thing.constructor.name) + ')'
-                ) 
-                + JSON.stringify(thing);
-      }
-      
-      return JSON.stringify(thing);
-   }
-
    if( !alwaysCalledWith(expected) ) {
    
       fail( 
