56bfc5d05f0f0cf738c058cc16107c238d973d58
Jim Higson <jim@wikizzle.org>
2013-08-06 18:09:17 +0100

    new built versions

--- dist/oboe.js
+++ dist/oboe.js
@@ -1778,47 +1778,46 @@ function oboeController(eventBus, clarinetParser, parsedContentSoFar) {
    };
    
    function apiMethod(httpMethodName, mayHaveRequestBody) {
-         
-      var 
-          // make name like 'doGet' out of name like 'GET'
-          bodyArgumentIndex =     mayHaveRequestBody?  1 : -1, // minus one = always undefined - method can't send data
-          callbackArgumentIndex = mayHaveRequestBody? 2 : 1;           
-      
-
+                  
       return function(firstArg){
       
          // wire everything up:
          var eventBus = pubSub(),
              clarinetParser = clarinet.parser(),
-             parsedContentSoFar = incrementalContentBuilder(clarinetParser, eventBus.notify),             
-             controller = oboeController( eventBus, clarinetParser, parsedContentSoFar),      
-            
-         // now work out what the arguments mean:   
-             url, body, doneCallback;
+             contentBuilder = incrementalContentBuilder(clarinetParser, eventBus.notify),             
+             controller = oboeController( eventBus, clarinetParser, contentBuilder),
 
+             /**
+              * create a shortcutted version of controller.start
+              */
+             start = controller.start.bind( controller, httpMethodName );
+             
          if (isString(firstArg)) {
+         
             // parameters specified as arguments
             //
-            //  if mayHaveContext, signature is:
+            //  if (mayHaveContext == true) method signature is:
             //     .method( url, content, callback )
+            //
             //  else it is:
             //     .method( url, callback )            
             //                                
-            url = firstArg;
-            body = arguments[bodyArgumentIndex];
-            doneCallback = arguments[callbackArgumentIndex]
+            start(   firstArg,                                       // url
+                     mayHaveRequestBody? arguments[1] : undefined,   // body
+                     arguments[mayHaveRequestBody? 2 : 1] );         // callback
          } else {
-            // parameters specified as options object:
-            url = firstArg.url;
-            body = firstArg.body;
-            doneCallback = firstArg.complete;
+         
+            
+            // method signature is:
+            //    .method({url:u, body:b, doneCallback:c})
+            
+            start(   firstArg.url,
+                     firstArg.body,
+                     firstArg.complete );
          }
-
-         // start the request:
-         controller.start(httpMethodName, url, body, doneCallback);         
-                  
+                                           
          // return an api to control this oboe instance                   
-         return instanceApi(controller, eventBus, parsedContentSoFar)           
+         return instanceApi(controller, eventBus, contentBuilder)           
       };
    }   
 
