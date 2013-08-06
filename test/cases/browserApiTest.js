4e35a3578519276c8782304aa6ca8ad33e431ccc
Jim Higson <jim@wikizzle.org>
2013-08-06 14:42:25 +0100

    Make failure message more lucid

--- test/cases/browserApiTest.js
+++ test/cases/browserApiTest.js
@@ -14,13 +14,48 @@ function streamingXhrShouldHaveBeenGiven(/* arguments */) {
    
       return streamingStub.alwaysCalledWithMatch.apply( streamingStub, expectedArgs );    
    }
+   
+   function anyToString(thing){
+      
+      if( thing === null ) {
+         return 'null';
+      }
+      
+      if( thing === undefined ) {
+         return 'undefined';
+      }
+
+      if( typeof thing == 'string' ) {
+         return '(string)' + thing;
+      }
+      
+      if( thing.constructor == Array ) {
+         return '[' + thing.map(anyToString).join(', ') + ']';
+      }      
+
+      if( typeof thing == 'function' ) {
+         return thing.name? 'function ' + thing.name : 'anon function'; 
+      }
+      
+      if( typeof thing == 'object' ) {
+            
+         return (
+                  thing.constructor == Object? 
+                     '' 
+                  :  '(' + (thing.constructor.name) + ')'
+                ) 
+                + JSON.stringify(thing);
+      }
+      
+      return JSON.stringify(thing);
+   }
 
    if( !alwaysCalledWith(expected) ) {
    
       fail( 'arguments given to streamingXhr not as expected. Wanted:',
-            JSON.stringify( expected ),
-            'but have calls:',
-            JSON.stringify(streamingStub.args)
+            anyToString(expected),
+            'but have (first recorded) call:',
+            anyToString(streamingStub.args[0])
       );
    }   
 }
