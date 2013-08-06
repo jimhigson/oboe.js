4710104d9e4f9b1a19daac8f84008a06faf2a54f
Jim Higson <jim@wikizzle.org>
2013-08-06 17:35:52 +0100

    fix test failure under Safari

--- src/browserApi.js
+++ src/browserApi.js
@@ -9,13 +9,7 @@
    };
    
    function apiMethod(httpMethodName, mayHaveRequestBody) {
-         
-      var 
-          // make name like 'doGet' out of name like 'GET'
-          bodyArgumentIndex =     mayHaveRequestBody?  1 : -1, // minus one = always undefined - method can't send data
-          callbackArgumentIndex = mayHaveRequestBody? 2 : 1;           
-      
-
+                  
       return function(firstArg){
       
          // wire everything up:
@@ -24,10 +18,16 @@
              parsedContentSoFar = incrementalContentBuilder(clarinetParser, eventBus.notify),             
              controller = oboeController( eventBus, clarinetParser, parsedContentSoFar),      
             
-         // now work out what the arguments mean:   
+         //  now work out what the arguments mean:   
              url, body, doneCallback;
 
          if (isString(firstArg)) {
+            /*console.log( 'apiMethod called', 
+                           anyToString(arguments), 
+                           bodyArgumentIndex, 
+                           arguments[-1] 
+            );*/
+         
             // parameters specified as arguments
             //
             //  if mayHaveContext, signature is:
@@ -36,8 +36,8 @@
             //     .method( url, callback )            
             //                                
             url = firstArg;
-            body = arguments[bodyArgumentIndex];
-            doneCallback = arguments[callbackArgumentIndex]
+            body = mayHaveRequestBody? arguments[1] : undefined;
+            doneCallback = arguments[mayHaveRequestBody? 2 : 1]
          } else {
             // parameters specified as options object:
             url = firstArg.url;
