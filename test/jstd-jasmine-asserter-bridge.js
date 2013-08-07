cc9168b845a13294c5725c8be9b2038d17b41fca
Jim Higson <jim@wikizzle.org>
2013-08-07 14:36:04 +0100

    +explain

--- test/jstd-jasmine-asserter-bridge.js
+++ test/jstd-jasmine-asserter-bridge.js
@@ -2,7 +2,8 @@
  * A lot of tests have been converted over from jstd. While I like the jasmine syntax better,
  * for a faster conversion a lot of the assertions have been kept in jstd style.
  * 
- * This file just bridges that over to jasmine style.
+ * This file just bridges that over to jasmine style. Future tests should use Jasmine directly
+ * and at some point I should convert them over.
  */
 
 function assertEquals(expected, actual) {
