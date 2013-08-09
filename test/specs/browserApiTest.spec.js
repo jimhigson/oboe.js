98be12c60384630133cbdff2a5c309f7a435b235
Jim Higson <jim@wikizzle.org>
2013-08-09 10:35:05 +0100

    +hierarchy

--- test/specs/browserApiTest.spec.js
+++ test/specs/browserApiTest.spec.js
@@ -14,116 +14,122 @@ describe("calls to browser api propagate to streaming xhr", function(){
    beforeEach(function() {
       spyOn(window, 'streamingXhr');      
    });
-   
-   // GET
-   it('can make a get', function(){   
-   
-      oboe.doGet('http://example.com/oboez', callbackPlaceholder)
-   
-      expect( streamingXhr ).toHaveBeenCalledWith(
-          'GET',
-          'http://example.com/oboez',
-          undefined,
-          jasmine.any(Function),
-          jasmine.any(Function)
-      )      
+
+   describe('get', function(){
+      
+      it('works via arguments', function(){   
+      
+         oboe.doGet('http://example.com/oboez', callbackPlaceholder)
+      
+         expect( streamingXhr ).toHaveBeenCalledWith(
+             'GET',
+             'http://example.com/oboez',
+             undefined,
+             jasmine.any(Function),
+             jasmine.any(Function)
+         )      
+            
+      })
          
-   })
-      
-   it('can make a get via options object', function(){   
-        
-      oboe.doGet({url: 'http://example.com/oboez', success: callbackPlaceholder})
-      
-      expect( streamingXhr ).toHaveBeenCalledWith(
-         'GET',
-         'http://example.com/oboez',
-         undefined,
-         jasmine.any(Function),
-         jasmine.any(Function)
-      )   
-   })   
-   
-   // DELETE
-   it('can make a delete', function(){
-        
-      oboe.doDelete('http://example.com/oboez', callbackPlaceholder)
-    
-      expect( streamingXhr ).toHaveBeenCalledWith(
-         'DELETE',
-         'http://example.com/oboez',
-         undefined,
-         jasmine.any(Function),
-         jasmine.any(Function)
-      )
-   })
-   
-   it('can make a delete via options object', function(){   
+      it('works via options object', function(){   
+           
+         oboe.doGet({url: 'http://example.com/oboez', success: callbackPlaceholder})
          
-      oboe.doDelete({url: 'http://example.com/oboez', success: callbackPlaceholder})
+         expect( streamingXhr ).toHaveBeenCalledWith(
+            'GET',
+            'http://example.com/oboez',
+            undefined,
+            jasmine.any(Function),
+            jasmine.any(Function)
+         )   
+      })
+         
+   });
+   
+   describe('delete', function(){
+      it('works via arguments', function(){
+           
+         oboe.doDelete('http://example.com/oboez', callbackPlaceholder)
+       
+         expect( streamingXhr ).toHaveBeenCalledWith(
+            'DELETE',
+            'http://example.com/oboez',
+            undefined,
+            jasmine.any(Function),
+            jasmine.any(Function)
+         )
+      })
       
-      expect( streamingXhr ).toHaveBeenCalledWith(
-         'DELETE',
-         'http://example.com/oboez',
-         undefined,
-         jasmine.any(Function),
-         jasmine.any(Function)
-      )   
-   })   
+      it('works via options object', function(){   
+            
+         oboe.doDelete({url: 'http://example.com/oboez', success: callbackPlaceholder})
+         
+         expect( streamingXhr ).toHaveBeenCalledWith(
+            'DELETE',
+            'http://example.com/oboez',
+            undefined,
+            jasmine.any(Function),
+            jasmine.any(Function)
+         )   
+      })   
+   });
      
          
-   // POST
-   it('can make a post', function(){
+   describe('post', function(){
+      it('works via arguments', function(){
+            
+         oboe.doPost('http://example.com/oboez', 'my_data', callbackPlaceholder)
          
-      oboe.doPost('http://example.com/oboez', 'my_data', callbackPlaceholder)
+         expect( streamingXhr ).toHaveBeenCalledWith(
+            'POST',
+            'http://example.com/oboez',
+            'my_data',
+            jasmine.any(Function),
+            jasmine.any(Function)
+         )   
+      })
       
-      expect( streamingXhr ).toHaveBeenCalledWith(
-         'POST',
-         'http://example.com/oboez',
-         'my_data',
-         jasmine.any(Function),
-         jasmine.any(Function)
-      )   
-   })
+      it('can post an object', function(){
+            
+         oboe.doPost('http://example.com/oboez', [1,2,3,4,5], callbackPlaceholder)
+         
+         expect( streamingXhr ).toHaveBeenCalledWith(
+            'POST',
+            'http://example.com/oboez',
+            [1,2,3,4,5],
+            jasmine.any(Function),
+            jasmine.any(Function)
+         )   
+      })   
+      
+      it('works via options object', function(){   
+            
+         oboe.doPost({url: 'http://example.com/oboez', body:'my_data', success: callbackPlaceholder})
+         
+         expect( streamingXhr ).toHaveBeenCalledWith(
+            'POST',
+            'http://example.com/oboez',
+            'my_data',
+            jasmine.any(Function),
+            jasmine.any(Function)
+         )   
+      })   
+   });
    
-   it('can make a can post an object', function(){
+   describe('put', function(){   
+      it('can put a string', function(){
+            
+         oboe.doPut('http://example.com/oboez', 'my_data', callbackPlaceholder)
          
-      oboe.doPost('http://example.com/oboez', [1,2,3,4,5], callbackPlaceholder)
-      
-      expect( streamingXhr ).toHaveBeenCalledWith(
-         'POST',
-         'http://example.com/oboez',
-         [1,2,3,4,5],
-         jasmine.any(Function),
-         jasmine.any(Function)
-      )   
-   })   
-   
-   it('can make a post via options object', function(){   
-         
-      oboe.doPost({url: 'http://example.com/oboez', body:'my_data', success: callbackPlaceholder})
-      
-      expect( streamingXhr ).toHaveBeenCalledWith(
-         'POST',
-         'http://example.com/oboez',
-         'my_data',
-         jasmine.any(Function),
-         jasmine.any(Function)
-      )   
-   })   
-   
-   // PUT   
-   it('can make a put', function(){
-         
-      oboe.doPut('http://example.com/oboez', 'my_data', callbackPlaceholder)
-      
-      expect( streamingXhr ).toHaveBeenCalledWith(
-         'PUT',
-         'http://example.com/oboez',
-         'my_data',
-         jasmine.any(Function),
-         jasmine.any(Function)
-      )   
-   })
+         expect( streamingXhr ).toHaveBeenCalledWith(
+            'PUT',
+            'http://example.com/oboez',
+            'my_data',
+            jasmine.any(Function),
+            jasmine.any(Function)
+         )   
+      })
+   });
       
 });
 
