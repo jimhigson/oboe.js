af38b13bca7d0fdaca490e9e33f2918ce9928444
Jim Higson <jim@wikizzle.org>
2013-08-14 11:03:17 +0100

    remove asserters from bridge now not being used

--- test/jstd-jasmine-asserter-bridge.js
+++ test/jstd-jasmine-asserter-bridge.js
@@ -1,54 +1,11 @@
 /*
- * A lot of tests have been converted over from jstd. While I like the jasmine syntax better,
- * for a faster conversion a lot of the assertions have been kept in jstd style.
+ * Before I used Karma/Jasmine I used JS Test Driver for the tests but I switched because
+ * it was too unstable.
  * 
- * This file just bridges that over to jasmine style. Future tests should use Jasmine directly
- * and at some point I should convert them over.
+ * JSTD provided a fail method to finish the test right away, emulate this by throwing an
+ * exception.
  */
 
-
-
-function assertEquals(a,b,c){
-
-   function assertEquals2(expected, actual) {
-      expect(actual).toEqual(expected);
-   }
-   
-   function assertEquals3(msg, expected, actual) {
-      expect(actual).toEqual(expected);
-   }
-
-   if( arguments.length == 2 ) {
-      assertEquals2(a,b);
-   } else if( arguments.length == 3 ) {
-      assertEquals3(a,b,c);
-   }
-
-}
-
-function assertTrue(a, b) {
-   if( arguments.length == 2 ) {
-   
-      expect(b).toBe(true);
-
-   } else {
-   
-      expect(a).toBe(true); 
-   }
-}
-
-function assertFalse(actual) {
-   expect(actual).toBe(false);
-}
 function fail(message) {
    throw new Error(message);
 }
-
-/**
- * While converting over, it is useful for tests not to fail because TestCase
- * isn't defined. Make no attempt to run the tests, just stub it out.
- * @param name
- * @param tests
- */
-function TestCase(name, tests){}
-function AsyncTestCase(name, tests){}
