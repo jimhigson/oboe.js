a952fdca71d43305f71a5776d1b7e7bfb0ab6a20
Jim Higson <jim@wikizzle.org>
2013-08-14 10:35:31 +0100

    change assert to expect

--- test/specs/functional.spec.js
+++ test/specs/functional.spec.js
@@ -3,23 +3,30 @@ describe("functional", function() {
 
    describe("varargs", function() {
    
-      it('works with fixed arguments', function() {
+      describe('with fixed arguments', function() {
             
-            var received1, received2, received3;
+            var received1, received2, receivedRest;
          
-            function f(r1, r2, r3){
-               received1 = r1;
-               received2 = r2;
-               received3 = r3;
+            function f(a1, a2, array){
+               received1 = a1;
+               received2 = a2;
+               receivedRest = array;
             }
          
             var varargsSpy = varArgs(f);
             
             varargsSpy('a', 'b', 'c', 'd', 'e', 'f');
             
-            assertEquals('a', received1);
-            assertEquals('b', received2);
-            assertEquals(['c', 'd', 'e', 'f'], received3);
+            it( 'passes through the first arguments as-is' , function() {
+            
+               expect(received1).toBe('a');
+               expect(received2).toBe('b');            
+            });
+            
+            it( 'passes through the rest as an array', function() {
+            
+               expect(receivedRest).toEqual(['c', 'd', 'e', 'f']);
+            });
       });
          
       it("works with no fixed arguments", function() {
@@ -34,7 +41,7 @@ describe("functional", function() {
          
          varargsSpy('a', 'b', 'c', 'd', 'e', 'f');
          
-         assertEquals(['a', 'b', 'c', 'd', 'e', 'f'], received1);
+         expect(received1).toEqual(['a', 'b', 'c', 'd', 'e', 'f']);
       });
          
               
@@ -42,7 +49,7 @@ describe("functional", function() {
                
          var varargsTestFn = varArgs(function(){ return 'expected' });
                   
-         assertEquals('expected', varargsTestFn());
+         expect( varargsTestFn() ).toBe('expected');                   
       });   
    });
    
@@ -56,19 +63,19 @@ describe("functional", function() {
          
          var composed = compose(dub, inc, half);  // composed(x) = dub(inc(half(x)))
          
-         assertEquals( 4 , composed(2)); // if this gives 2.5 the order is wrong      
+         expect(composed(2)).toBe( 4 ); // if this gives 2.5 the order is wrong      
       });
       
       it('can compose head to take an item off a list and then use it', function() {
          var list = cons( {a:1}, emptyList );
    
-         assertEquals(1, compose(attr('a'), head)( list ));      
+         expect( compose(attr('a'), head)( list ) ).toBe(1);      
       });
       
       it('gives an identity function when making a composition of zero functions', function() {
          var id = compose();
          
-         assertEquals( 2 , id(2));      
+         expect( id(2) ).toBe(2);      
       });
        
    });
