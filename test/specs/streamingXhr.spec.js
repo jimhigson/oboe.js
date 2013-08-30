dc9a7117a3db88d714ec558debe16514e67ab43c
Jim Higson <jim@wikizzle.org>
2013-08-30 13:14:19 +0100

    test sXHR propagates abort through to XHR

--- test/specs/streamingXhr.spec.js
+++ test/specs/streamingXhr.spec.js
@@ -1,7 +1,7 @@
 
 describe("streamingXhr", function(){
 
-   describe("requests should get through to browser xhr correctly", function(){
+   describe("calls get through to browser xhr correctly", function(){
                                
       var notifyStub = jasmine.createSpy('eventBus.notify');                                 
                                
@@ -9,21 +9,21 @@ describe("streamingXhr", function(){
       
          streamingXhr(notifyStub).req('GET', 'http://example.com', undefined);
          
-         expect( lastCreatedXhrInstance.send ).toHaveBeenGiven(null);
+         expect( lastCreatedXhrInstance.send ).toHaveBeenGivenBody(null);
       })
       
       it('gives xhr null when body is null', function(){
       
          streamingXhr(notifyStub).req('GET', 'http://example.com', null);
          
-         expect( lastCreatedXhrInstance.send ).toHaveBeenGiven(null);
+         expect( lastCreatedXhrInstance.send ).toHaveBeenGivenBody(null);
       })      
       
       it('give xhr string request body', function(){
       
          streamingXhr(notifyStub).req('GET', 'http://example.com', 'my_data');
          
-         expect( lastCreatedXhrInstance.send ).toHaveBeenGiven('my_data');
+         expect( lastCreatedXhrInstance.send ).toHaveBeenGivenBody('my_data');
       })
       
       it('gives xhr a json encoded request body when given an object', function(){
@@ -32,23 +32,35 @@ describe("streamingXhr", function(){
          
          streamingXhr(notifyStub).req('GET', 'http://example.com', payload);
          
-         expect( lastCreatedXhrInstance.send ).toHaveBeenGiven(JSON.stringify( payload ) );      
+         expect( lastCreatedXhrInstance.send ).toHaveBeenGivenBody(JSON.stringify( payload ) );      
       });      
+      
+      it('should be able to abort an xhr once started', function(){
+         var sXhr = streamingXhr(notifyStub);
+         
+         sXhr.req('GET', 'http://example.com', 'my_data');
+         
+         sXhr.abort();
+                  
+         expect( lastCreatedXhrInstance.abort.called ).toBe(true);                  
+      });
 
 
       var FakeXhrClass, lastCreatedXhrInstance;      
 
       beforeEach( function() {
+      
          FakeXhrClass = sinon.useFakeXMLHttpRequest();
          
          FakeXhrClass.onCreate = function(xhr) {
             lastCreatedXhrInstance = xhr;
             sinon.spy(lastCreatedXhrInstance, 'send');
+            sinon.spy(lastCreatedXhrInstance, 'abort');
          };      
          
          this.addMatchers({
          
-            toHaveBeenGiven:function( expectedBody ) {
+            toHaveBeenGivenBody:function( expectedBody ) {
                var sendMethod = this.actual;
                
                return sendMethod.firstCall.args[0] == expectedBody;               
