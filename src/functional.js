e1c39d8e8df72033d93b78fcd72ce908635a7e2c
Jim Higson <jim@wikizzle.org>
2013-09-23 14:27:35 +0100

    tidy up comments

--- src/functional.js
+++ src/functional.js
@@ -45,15 +45,15 @@ function varArgs(fn){
 }
 
 /**
- *  Call a list of functions with the same args until one returns a truthy result. Equivalent to || in javascript
+ * Call a list of functions with the same args until one returns a truthy result. Equivalent to || in javascript
+ * 
+ * So:
+ *      lazyUnion([f1,f2,f3 ... fn])( p1, p2 ... pn )
+ *      
+ * Is equivalent to: 
+ *      apply(f1, [p1, p2 ... pn]) || apply(f2, [p1, p2 ... pn]) || apply(f3, [p1, p2 ... pn]) ... apply(fn, [p1, p2 ... pn])  
  *  
- *  So:
- *       lazyUnion([f1,f2,f3 ... fn])( p1, p2 ... pn )
- *       
- *  Is equivalent to: 
- *       apply(f1, [p1, p2 ... pn]) || apply(f2, [p1, p2 ... pn]) || apply(f3, [p1, p2 ... pn]) ... apply(fn, [p1, p2 ... pn])  
- *   
- *  @returns the first return value that is given that is truthy.
+ * @returns the first return value that is given that is truthy.
  */
 var lazyUnion = varArgs(function(fns) {
 
@@ -85,8 +85,9 @@ function lazyIntersection(fn1, fn2) {
    };   
 }
 
-/** Partially complete the given function by filling it in with all arguments given
- *  after the function itself. Returns the partially completed version.
+/** 
+ * Partially complete the given function by filling it in with all arguments given
+ * after the function itself. Returns the partially completed version.
  */
 var partialComplete = varArgs(function( fn, boundArgs ) {
 
