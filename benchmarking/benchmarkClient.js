0e23e4803d7df1f5813838ccbe9f985cc719d154
Jim Higson <jim@wikizzle.org>
2013-10-12 16:07:44 +0100

    got first benchmark not crashing now

--- benchmarking/benchmarkClient.js
+++ benchmarking/benchmarkClient.js
@@ -3,6 +3,33 @@ require('color');
 
 var DB_URL = 'localhost:4444/db';  
 
+function oboeReqInner(record) {
+
+   var oboe = require('../dist/oboe-node.js');
+
+   var url = record.url;
+                       
+   console.log('will make inner request to', url);
+              
+   // we have a record. Now get the item linked to:
+   
+   try {
+   
+      oboe(url).node('name', function(name){
+      
+         console.log(name);
+         //this.abort();                           
+      }).fail(function(e){
+      
+         console.log('error making request', url, e);            
+      });
+      
+   } catch(e){
+      console.log('!--- unpected error', e);
+   }
+      
+}
+
 function aggregateWithOboe() {
 
    var oboe = require('../dist/oboe-node.js');
@@ -20,22 +47,7 @@ function aggregateWithOboe() {
    console.log('making request to ', DB_URL);
    
    oboe(DB_URL)
-      .node('{id url}', function(record){
-                     
-         /*console.log('will make ongoing request to', record.url);
-                    
-         // we have a record. Now get the item linked to:
-         
-         oboe(record.url).node('name', function(name){
-         
-            console.log(name);
-            this.abort();                           
-         }).error(function(e){
-         
-            console.log('error making request', record.url, e);            
-         });*/
-         
-      })
+      .node('{id url}', oboeReqInner)
       .fail(function(){
          console.log('error in db request', e);
       });
