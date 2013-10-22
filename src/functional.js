11821363fc584d7438bcea3665886f0b92c2a18a
Jim Higson <jim@wikizzle.org>
2013-10-22 11:43:30 +0100

    fix comment:
    https://github.com/jimhigson/oboe.js/issues/17

--- src/functional.js
+++ src/functional.js
@@ -2,9 +2,17 @@
  * Partially complete a function.
  * 
  * Eg: 
- *    var add3 = partialComplete( function add(a,b){return a+b}, [3] );
+ *    var add3 = partialComplete( function add(a,b){return a+b}, 3 );
  *    
  *    add3(4) // gives 7
+ *    
+ *    
+ *    function wrap(left, right, cen){return left + " " + cen + " " + right;}
+ *    
+ *    var pirateGreeting = partialComplete( wrap , "I'm", ", a mighty pirate!" );
+ *    
+ *    pirateGreeting("Guybrush Threepwood"); 
+ *                         // gives "I'm Guybrush Threepwood, a mighty pirate!"
  */
 var partialComplete = varArgs(function( fn, boundArgs ) {
 
