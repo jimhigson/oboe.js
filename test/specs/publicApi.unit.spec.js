d1ed996204bedf61d8fa4e88ce242b43439e3759
Jim Higson <jim@wikizzle.org>
2013-11-01 11:48:15 +0000

    move beforeEach to end of spec file

--- test/specs/publicApi.unit.spec.js
+++ test/specs/publicApi.unit.spec.js
@@ -10,52 +10,6 @@
 describe("public api", function(){
    "use strict";
 
-
-   this.beforeEach(function(){
-   
-      this.addMatchers({
-         /* Under Jasmine's toHaveBeenCalledLike, subject(foo, undefined)
-            is considered different from subject(foo). This is slightly
-            looser and considers those equal.  
-          */
-         toHaveBeenCalledLike:function(/*expectedArgs*/){
-            var expectedArgs = Array.prototype.slice.apply(arguments);
-            var actualCalls = this.actual.calls;
-            
-            var equals = this.env.equals_.bind(this.env);
-            
-            this.message = function() {
-               var invertedMessage = "Expected spy " + this.actual.identity + " not to have been called like " + jasmine.pp(expectedArgs) + " but it was.";
-               var positiveMessage = "";
-               if (this.actual.callCount === 0) {
-                  positiveMessage = "Expected spy " + this.actual.identity + " to have been called like " + jasmine.pp(expectedArgs) + " but it was never called.";
-               } else {
-                  positiveMessage = "Expected spy " + this.actual.identity + " to have been called like " + jasmine.pp(expectedArgs) + " but actual calls were " + jasmine.pp(this.actual.argsForCall).replace(/^\[ | \]$/g, '')
-               }
-               return [positiveMessage, invertedMessage];
-            };            
-                        
-            return actualCalls.some(function( actualCall ){
-            
-               var actualArgs = actualCall.args;
-
-               // check for one too many arguments given. But this is ok
-               // if the extra arg is undefined.               
-               if( actualArgs[expectedArgs.length] != undefined ) {
-
-                  return false;
-               }
-               
-               return expectedArgs.every(function( expectedArg, index ){
-                  
-                  return equals( actualArgs[index], expectedArg );                  
-               });
-            
-            });
-         }
-      });
-   })
-
    describe("propagates through to wiring function", function(){
   
       beforeEach(function() {
@@ -190,6 +144,51 @@ describe("public api", function(){
       })
       
    });
+   
+   this.beforeEach(function(){
+   
+      this.addMatchers({
+         /* Under Jasmine's toHaveBeenCalledLike, subject(foo, undefined)
+            is considered different from subject(foo). This is slightly
+            looser and considers those equal.  
+          */
+         toHaveBeenCalledLike:function(/*expectedArgs*/){
+            var expectedArgs = Array.prototype.slice.apply(arguments);
+            var actualCalls = this.actual.calls;
+            
+            var equals = this.env.equals_.bind(this.env);
+            
+            this.message = function() {
+               var invertedMessage = "Expected spy " + this.actual.identity + " not to have been called like " + jasmine.pp(expectedArgs) + " but it was.";
+               var positiveMessage = "";
+               if (this.actual.callCount === 0) {
+                  positiveMessage = "Expected spy " + this.actual.identity + " to have been called like " + jasmine.pp(expectedArgs) + " but it was never called.";
+               } else {
+                  positiveMessage = "Expected spy " + this.actual.identity + " to have been called like " + jasmine.pp(expectedArgs) + " but actual calls were " + jasmine.pp(this.actual.argsForCall).replace(/^\[ | \]$/g, '')
+               }
+               return [positiveMessage, invertedMessage];
+            };            
+                        
+            return actualCalls.some(function( actualCall ){
+            
+               var actualArgs = actualCall.args;
+
+               // check for one too many arguments given. But this is ok
+               // if the extra arg is undefined.               
+               if( actualArgs[expectedArgs.length] != undefined ) {
+
+                  return false;
+               }
+               
+               return expectedArgs.every(function( expectedArg, index ){
+                  
+                  return equals( actualArgs[index], expectedArg );                  
+               });
+            
+            });
+         }
+      });
+   })   
       
 });
 
