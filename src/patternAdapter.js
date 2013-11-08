ecc774c4617a3120cb980e6a814744b6b7f17a96
Jim Higson <jim@wikizzle.org>
2013-11-08 13:01:15 +0000

    initial pattern adaptor

---
+++ src/patternAdapter.js
@@ -0,0 +1,61 @@
+function patternAdapter(bus, jsonPathCompiler) {
+
+   function addUnderlyingListener( matchEventName, predicateEventName, pattern ){
+
+      var compiledJsonPath = jsonPathCompiler( pattern );
+   
+      bus.on(predicateEventName, function (ascent) {
+
+         var maybeMatchingMapping = compiledJsonPath(ascent);
+
+         /* Possible values for maybeMatchingMapping are now:
+
+          false: 
+          we did not match 
+
+          an object/array/string/number/null: 
+          we matched and have the node that matched.
+          Because nulls are valid json values this can be null.
+
+          undefined:
+          we matched but don't have the matching node yet.
+          ie, we know there is an upcoming node that matches but we 
+          can't say anything else about it. 
+          */
+         if (maybeMatchingMapping !== false) {
+             
+            bus.emit(matchEventName, nodeOf(maybeMatchingMapping), ascent);
+         }
+      }, matchEventName);
+   
+   
+      bus.on('removeListener', function(removedEventName){
+   
+         // if the match even listener is later removed, clean up by removing
+         // the underlying listener if nothing else is using that pattern:
+      
+         if( removedEventName == matchEventName ) {
+         
+            if( !bus.listeners( removedEventName )) {
+               bus.un( predicateEventName, matchEventName );
+            }
+         }
+      });   
+   }
+
+   bus.on('newListener', function(matchEventName){
+
+      var match = /(\w+):(.*)/.exec(matchEventName),
+          predicateEventName = match && {node:NODE_FOUND, path:PATH_FOUND}[match[1]];
+                    
+      if( predicateEventName && !bus.hasListener(predicateEventName, matchEventName) ) {  
+               
+         addUnderlyingListener(
+            matchEventName,
+            predicateEventName, 
+            match[2]
+         );
+      }    
+   })
+
+}
