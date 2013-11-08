843b3c6b8f4d3f36c2352ebb8d3d93f4f416185c
Jim Higson <jim@wikizzle.org>
2013-11-08 15:55:20 +0000

    get a tiny bit more performance out

--- dist/oboe-browser.js
+++ dist/oboe-browser.js
@@ -140,13 +140,31 @@ function apply(args, fn) {
 function varArgs(fn){
 
    var numberOfFixedArguments = fn.length -1,
-       slice = Array.prototype.slice,
-       
-       // we know how many arguments fn will always take, so create a
-       // fixed-size array to hold that many, to be re-used to
-       // avoid creating a new array:
-       argsHolder = Array(fn.length);
-                
+       slice = Array.prototype.slice;          
+         
+                   
+   if( numberOfFixedArguments == 0 ) {
+      // an optimised case for when there are no fixed args:   
+   
+      return function(){
+         return fn.call(this, slice.call(arguments));
+      }
+      
+   } else if( numberOfFixedArguments == 1 ) {
+      // an optimised case for when there are is one fixed args:
+   
+      return function(){
+         return fn.call(this, arguments[0], slice.call(arguments, 1));
+      }
+   }
+   
+   // general case   
+
+   // we know how many arguments fn will always take. Create a
+   // fixed-size array to hold that many, to be re-used on
+   // every call to the returned function
+   var argsHolder = Array(fn.length);   
+                             
    return function(){
                             
       for (var i = 0; i < numberOfFixedArguments; i++) {
