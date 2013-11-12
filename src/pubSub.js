0e54f3e62384366737abc79d1691670d1b9566b1
Jim Higson <jim@wikizzle.org>
2013-11-12 13:21:36 +0000

    remove blank line

--- src/pubSub.js
+++ src/pubSub.js
@@ -58,7 +58,6 @@ function pubSub(){
       pubSubInstance[methodName] = varArgs(function(eventName, parameters){
          apply( parameters, pubSubInstance( eventName )[methodName]);
       });   
-   
    })
          
    return pubSubInstance;
