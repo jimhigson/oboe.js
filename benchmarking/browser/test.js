344f8f50c2faa6b471ddcdf377bc5ef6f199f5ca
Jim Higson <jim@wikizzle.org>
2013-11-13 16:39:05 +0000

    split running benchmarks into function and call

--- benchmarking/browser/test.js
+++ benchmarking/browser/test.js
@@ -6,83 +6,86 @@ var numberOfRecords = 100;
 var testName = numberOfRuns + ' big downloads';
 var profile = true;        
 
-function generateTestJson(){
+function go(oboe){
 
-   var container = {
-       "id": 1,
-       "jsonrpc": "2.0",
-       "total": 20000,
-       "result": [] 
-   };
-
-   for (var i = 0; i < numberOfRecords; i++) {
-      container.result.push({
-         "id": i,
-         "guid": "046447ee-da78-478c-b518-b612111942a5",
-         "picture": "http://placehold.it/32x32",
-         "age": i,
-         "name": "Payton Murphy",
-         "company": "Robotomic",
-         "phone": "806-587-2379",
-         "email": "payton@robotomic.com"               
-      });
-   }
-   return JSON.stringify(container);
-}
-
-runTest( generateTestJson() );         
-                        
-function runTest(content) {
-        
-   console.log('will start testing in', initialWait, '...');
-         
-   window.setTimeout( function() {
+   function generateTestJson(){
    
-      profile && console.profile(testName);
-      console.time(testName);
-         
-      perform(numberOfRuns);
-   }, initialWait );         
-
-   function perform(times){
-    
-      var idTotal = 0,
-          ageTotal = 0,
-          nodeCount = 0,
-          instance;
-                            
-      instance = oboe()
-         .node('!.$result..{age name company}', function(obj){
-          
-            nodeCount++;
-            idTotal += obj.id;                   
-         })
-         .path('age', function(age){
-
-            nodeCount++;                                 
-            ageTotal += age;       
-         })               
-         .done(function(){
-            if( nodeCount != 200 ) {
-               throw "wrong number of matches";
-            }
-             
-            if( times == 0 ) {
-               profile && console.profileEnd(testName);            
-               console.timeEnd(testName);
-            } else {
-               perform(times-1);
-            }            
-         }).fail(function(e){
-            console.log('there was a failure' + JSON.stringify(e));
+      var container = {
+          "id": 1,
+          "jsonrpc": "2.0",
+          "total": 20000,
+          "result": [] 
+      };
+   
+      for (var i = 0; i < numberOfRecords; i++) {
+         container.result.push({
+            "id": i,
+            "guid": "046447ee-da78-478c-b518-b612111942a5",
+            "picture": "http://placehold.it/32x32",
+            "age": i,
+            "name": "Payton Murphy",
+            "company": "Robotomic",
+            "phone": "806-587-2379",
+            "email": "payton@robotomic.com"               
          });
-                     
-      // pass in a drip at a time            
-      for(     var dripStart = 0, dripEnd = progressAtOnce; 
-               dripStart < content.length; 
-               dripStart += progressAtOnce, dripEnd += progressAtOnce ) {
-                                    
-         instance.emit( 'content', content.substring(dripStart, dripEnd));
-      }               
+      }
+      return JSON.stringify(container);
+   }
+   
+   runTest( generateTestJson() );         
+                           
+   function runTest(content) {
+           
+      console.log('will start testing in', initialWait, '...');
+            
+      window.setTimeout( function() {
+      
+         profile && console.profile(testName);
+         console.time(testName);
+            
+         perform(numberOfRuns);
+      }, initialWait );         
+   
+      function perform(times){
+       
+         var idTotal = 0,
+             ageTotal = 0,
+             nodeCount = 0,
+             instance;
+                               
+         instance = oboe()
+            .node('!.$result..{age name company}', function(obj){
+             
+               nodeCount++;
+               idTotal += obj.id;                   
+            })
+            .path('age', function(age){
+   
+               nodeCount++;                                 
+               ageTotal += age;       
+            })               
+            .done(function(){
+               if( nodeCount != 200 ) {
+                  throw "wrong number of matches";
+               }
+                
+               if( times == 0 ) {
+                  profile && console.profileEnd(testName);            
+                  console.timeEnd(testName);
+               } else {
+                  perform(times-1);
+               }            
+            }).fail(function(e){
+               console.log('there was a failure' + JSON.stringify(e));
+            });
+                        
+         // pass in a drip at a time            
+         for(     var dripStart = 0, dripEnd = progressAtOnce; 
+                  dripStart < content.length; 
+                  dripStart += progressAtOnce, dripEnd += progressAtOnce ) {
+                                       
+            instance.emit( 'content', content.substring(dripStart, dripEnd));
+         }               
+      }
    }
 }
