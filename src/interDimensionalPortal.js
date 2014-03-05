fd82aa6c89e7e7a6361030f6312e5f153736e53d
Jim Higson <jim.higson@gmail.com>
2014-03-05 15:35:14 +0000

    keep code in worker as an array until it goes into the blob

--- src/interDimensionalPortal.js
+++ src/interDimensionalPortal.js
@@ -44,21 +44,19 @@ var portal = (function(){
    function codeForChildThread(childLibs, childServer, eventsFromChild) {
 
       return childLibs
-                  .concat(forward, receive)
-                  .map(String)
-                  .join('') +
-             'var bus=pubSub();' +
-             'forward(bus,' + JSON.stringify(eventsFromChild) + ');' +
-             'receive(bus);' + 
-             '(' + String(childServer) + '(bus))';
+                  .concat(forward, receive).map(String)
+                  .concat(
+                     'var bus=pubSub();',
+                     'forward(bus,' + JSON.stringify(eventsFromChild) + ');',
+                     'receive(bus);',
+                     '(' + String(childServer) + '(bus))');
    }
 
    return function (childLibs, childServer, parentThreadBus, eventsToChild, eventsFromChild){
 
-      var code = codeForChildThread(childLibs, childServer, eventsFromChild);
-     
-      // http://developer.mozilla.org/en-US/docs/Web/API/Blob
-      var blob = new Blob([code], {type:'text/javascript'}),
+      var code = codeForChildThread(childLibs, childServer, eventsFromChild),
+          // http://developer.mozilla.org/en-US/docs/Web/API/Blob
+          blob = new Blob(code, {type:'text/javascript'}),
           worker = new Worker(window.URL.createObjectURL(blob));
          
       forward(parentThreadBus, eventsToChild, worker);
