7497ccbfe13b9ad2ec2896c5620a39846fa81b5c
Jim Higson <jim.higson@gmail.com>
2014-03-15 17:51:10 +0000

    make importing scripts into tests less brittle

--- src/workerEnv.raw.js
+++ src/workerEnv.raw.js
@@ -14,45 +14,46 @@
  * @param {Function} f
  * @returns {String}
  */
-function defunctionise(f){
+function stringifyFunctionForWorker(f){
 
-   return String(f)
-      .replace(/^function\s*\(\s*\)\s*{/, '')
-      .replace(/\s*}\s*$/, '');
+   // The worker thread won't be able to see which domain the parent was on
+   // so we add it in hardcoded into its code, see?
+   
+   // Note that /base is how Karma makes js under test available.
+   
+   var location = window.location,
+       base = location.protocol + location.host + '/base';
+   
+   return '(' + String(f) + '("' + base + '"));';
 }
 
 
 var WORKER_ENV = [
-   defunctionise(function(){
+   stringifyFunctionForWorker(function(root){
 
-      // Note that /base is how Karma makes js under test available.
-      // The unminified source is only ever loaded for testing via Karma.
-      
-      // TODO: this will break when not running locally
-      
-      console.log('child thread: importing scripts...');
+      console.log('child thread: importing scripts from domain ' + root);
 
       importScripts( 
-         'http://localhost:9876/base/src/functional.js'
-      ,  'http://localhost:9876/base/src/util.js'
-      ,  'http://localhost:9876/base/src/lists.js' 
-      ,  'http://localhost:9876/base/src/libs/clarinet.js'
-      ,  'http://localhost:9876/base/src/ascentManager.js'
-      ,  'http://localhost:9876/base/src/parseResponseHeaders.browser.js'
-      ,  'http://localhost:9876/base/src/streamingHttp.browser.js'
-      ,  'http://localhost:9876/base/src/jsonPathSyntax.js'
-      ,  'http://localhost:9876/base/src/ascent.js'
-      ,  'http://localhost:9876/base/src/incrementalContentBuilder.js'
-      ,  'http://localhost:9876/base/src/jsonPath.js'
-      ,  'http://localhost:9876/base/src/singleEventPubSub.js'
-      ,  'http://localhost:9876/base/src/pubSub.js'
-      ,  'http://localhost:9876/base/src/events.js'
-      ,  'http://localhost:9876/base/src/patternAdapter.js'
-      ,  'http://localhost:9876/base/src/instanceApi.js'
-      ,  'http://localhost:9876/base/src/wire.js'
-      ,  'http://localhost:9876/base/src/defaults.js'
-      ,  'http://localhost:9876/base/src/publicApi.js'
-      ,  'http://localhost:9876/base/src/interDimensionalPortal.js'
+         root + '/src/functional.js'
+      ,  root + '/src/util.js'
+      ,  root + '/src/lists.js' 
+      ,  root + '/src/libs/clarinet.js'
+      ,  root + '/src/ascentManager.js'
+      ,  root + '/src/parseResponseHeaders.browser.js'
+      ,  root + '/src/streamingHttp.browser.js'
+      ,  root + '/src/jsonPathSyntax.js'
+      ,  root + '/src/ascent.js'
+      ,  root + '/src/incrementalContentBuilder.js'
+      ,  root + '/src/jsonPath.js'
+      ,  root + '/src/singleEventPubSub.js'
+      ,  root + '/src/pubSub.js'
+      ,  root + '/src/events.js'
+      ,  root + '/src/patternAdapter.js'
+      ,  root + '/src/instanceApi.js'
+      ,  root + '/src/wire.js'
+      ,  root + '/src/defaults.js'
+      ,  root + '/src/publicApi.js'
+      ,  root + '/src/interDimensionalPortal.js'
       );
 
       console.log('...importing scripts done');
