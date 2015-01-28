ce460630cfd1dd2d837a334101db4432c49e480d
ScreenDriver <github@echooff.de>
2015-01-28 16:16:04 +0100

    Added web worker support

--- src/wrapper.browser.js
+++ src/wrapper.browser.js
@@ -1,7 +1,15 @@
 // This file is the concatenation of many js files.
 // See http://github.com/jimhigson/oboe.js for the raw source
 
-// having a local undefined, window, Object etc allows slightly better minification:                    
+// Access to the window object throws an exception in HTML5 web workers so
+// point it to "self" if it runs in a web worker
+var win;
+try {
+  win = window;
+} catch (e) {
+  win = self;
+}
+// having a local undefined, window, Object etc allows slightly better minification:
 (function  (window, Object, Array, Error, JSON, undefined ) {
 
    // ---contents--- //
@@ -13,4 +21,4 @@
    } else {
       window.oboe = oboe;
    }
-})(window, Object, Array, Error, JSON);
+})(win, Object, Array, Error, JSON);
