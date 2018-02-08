bd2bdfda7714e9eacc80a4f5171944d634ecc11b
Ukor Jidechi <ukorjidechi@gmail.com>
2018-02-08 01:38:56 +0100

    Fix issue#146 header content-lenght should be fixed by browser

--- dist/oboe-browser.js
+++ dist/oboe-browser.js
@@ -2606,7 +2606,9 @@ function applyDefaults( passthrough, url, httpMethodName, body, headers, withCre
          // Default Content-Type to JSON unless given otherwise.
          headers['Content-Type'] = headers['Content-Type'] || 'application/json';
       }
-      headers['Content-Length'] = headers['Content-Length'] || body.length;
+       //Issue #146 (https://github.com/jimhigson/oboe.js/issues/146)
+      //see https://stackoverflow.com/a/7210840/3501729
+      //headers['Content-Length'] = headers['Content-Length'] || body.length;
    } else {
       body = null;
    }
