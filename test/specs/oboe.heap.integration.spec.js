7d917a53a5e62e1e260c81d32945fd4528d9a120
Aodhagan Collins <aodhaganc@gmail.com>
2018-03-19 00:45:13 +0000

    test(heap): remove chrome check

--- test/specs/oboe.heap.integration.spec.js
+++ test/specs/oboe.heap.integration.spec.js
@@ -6,34 +6,32 @@
     var oboe = window.oboe
     var testUrl = window.testUrl
 
-    if (Platform.isChrome) {
-      describe('heap tests', function () {
-        it('should not grow memory by more than expected', function (done) {
-          var startHeap = window.performance.memory.usedJSHeapSize
+    describe('heap tests', function () {
+      it('should not grow memory by more than expected', function (done) {
+        var startHeap = window.performance.memory.usedJSHeapSize
 
-          var nodes = []
-          oboe({
-            url: testUrl('largeResponse?limit=10000')
+        var nodes = []
+        oboe({
+          url: testUrl('largeResponse?limit=10000')
+        })
+          .node('!.*', function (node) {
+            nodes.push(node)
+            return oboe.drop
           })
-            .node('!.*', function (node) {
-              nodes.push(node)
-              return oboe.drop
-            })
-            .fail(function (error) {
-              console.error(error.thrown)
-              done(error.thrown)
-            })
-            .done(function (fullResponse) {
-              expect(fullResponse.length).toBe(10000)
-              var endHeap = window.performance.memory.usedJSHeapSize
-              console.log('Start Heap: ', startHeap)
-              console.log('End Heap: ', endHeap)
-              // when this fails this is by a factor in the hundreds
-              expect(((endHeap - startHeap) / startHeap)).toBeLessThan(15)
-              done()
-            })
-        }, 30000)
-      })
-    }
+          .fail(function (error) {
+            console.error(error.thrown)
+            done(error.thrown)
+          })
+          .done(function (fullResponse) {
+            expect(fullResponse.length).toBe(10000)
+            var endHeap = window.performance.memory.usedJSHeapSize
+            console.log('Start Heap: ', startHeap)
+            console.log('End Heap: ', endHeap)
+            // when this fails this is by a factor in the hundreds
+            expect(((endHeap - startHeap) / startHeap)).toBeLessThan(15)
+            done()
+          })
+      }, 30000)
+    })
   })
 })(typeof Platform === 'undefined' ? require('../libs/platform.js') : Platform)
